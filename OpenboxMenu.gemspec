9ea93be964529c4e73bdb29214d2d314ad093a6b
Ghardo <jerturo@gmail.com>
2013-10-10 20:03:10 +0200

    checkin

---
+++ OpenboxMenu.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'OpenboxMenu'
+  s.version     = '0.0.1'
+  s.date        = '2013-10-10'
+  s.summary     = "Openbox Menu Class"
+  s.description = "A simple class to generate Openbox Menu"
+  s.authors     = ['Stefan Martin']
+  s.email       = 'jerturo@gmail.com'
+  s.files       = ["lib/OpenboxMenu.rb"]
+  s.homepage    = 'https://github.com/Ghardo/ruby-openbox-menu.git'
+  s.license     = 'gpl'
+end
---
+++ lib/OpenboxMenu.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'nokogiri'
+
+class OpenboxMenu
+	
+	def initialize()
+		@doc = Nokogiri::XML('<openbox_pipe_menu></openbox_pipe_menu>')
+		@menu = Hash.new
+		@items = Hash.new
+	end
+	
+	def menu(menuid, label)
+		@menu[menuid] = Nokogiri::XML::Node.new( "menu", @doc )
+		@menu[menuid]['id'] = menuid
+		@menu[menuid]['label'] = label
+		@doc.root.add_child(@menu[menuid])
+	end
+	
+	def item(menuid, itemid, itemlabel)
+		@items[itemid] = Nokogiri::XML::Node.new( "item", @doc )
+		@items[itemid]['label'] = itemlabel
+		@menu[menuid].add_child(@items[itemid])
+	end
+	
+	def execute(itemid, command)
+		action = Nokogiri::XML::Node.new( "action", @doc )
+		action['name'] = 'Execute'
+		execute = Nokogiri::XML::Node.new( "execute", @doc )
+		execute.content = command
+		action.add_child(execute)
+		
+		@items[itemid].add_child(action)
+	end
+	
+	def getXml
+		return @doc.root.to_xml
+	end
+end
